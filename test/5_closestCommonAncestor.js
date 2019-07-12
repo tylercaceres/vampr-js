9b70eb3921a0de5199cc657513b0bdbcbf871105
tylerccaceres <tyler.caceres@gmail.com>
2019-07-12 19:34:46 -0400

    moving on to last section

--- test/5_closestCommonAncestor.js
+++ test/5_closestCommonAncestor.js
@@ -3,25 +3,23 @@ const expect = chai.expect;
 
 const Vampire = require('../vampire.js');
 
-describe("Vampire", function() {
-
+describe('Vampire', function() {
   let rootVampire;
-  beforeEach( function() {
-    rootVampire = new Vampire("root");
+  beforeEach(function() {
+    rootVampire = new Vampire('root');
   });
 
-  describe("closestCommonAncestor", function() {
-
+  describe('closestCommonAncestor', function() {
     let offspring1, offspring2, offspring3, offspring4, offspring5, offspring6, offspring7, offspring8;
     beforeEach(() => {
-      offspring1 = new Vampire("a");
-      offspring2 = new Vampire("b");
-      offspring3 = new Vampire("c");
-      offspring4 = new Vampire("d");
-      offspring5 = new Vampire("e");
-      offspring6 = new Vampire("f");
-      offspring7 = new Vampire("g");
-      offspring8 = new Vampire("h");
+      offspring1 = new Vampire('a');
+      offspring2 = new Vampire('b');
+      offspring3 = new Vampire('c');
+      offspring4 = new Vampire('d');
+      offspring5 = new Vampire('e');
+      offspring6 = new Vampire('f');
+      offspring7 = new Vampire('g');
+      offspring8 = new Vampire('h');
 
       rootVampire.addOffspring(offspring1);
       rootVampire.addOffspring(offspring2);
@@ -33,32 +31,32 @@ describe("Vampire", function() {
       offspring2.addOffspring(offspring8);
     });
 
-    it("should be the root vampire for any vampire and the root vampire", () => {
+    it('should be the root vampire for any vampire and the root vampire', () => {
       expect(rootVampire.closestCommonAncestor(offspring2).name).to.equal(rootVampire.name);
       expect(rootVampire.closestCommonAncestor(offspring7).name).to.equal(rootVampire.name);
-    })
+    });
 
-    it("should be the root vampire for first two offspring", () => {
+    it('should be the root vampire for first two offspring', () => {
       expect(offspring1.closestCommonAncestor(offspring2).name).to.equal(rootVampire.name);
-    })
+    });
 
-    it("should be offspring 3 for offspring 4 and 7", () => {
+    it('should be offspring 3 for offspring 4 and 7', () => {
       expect(offspring4.closestCommonAncestor(offspring7).name).to.equal(offspring3.name);
       expect(offspring7.closestCommonAncestor(offspring4).name).to.equal(offspring3.name);
-    })
+    });
 
-    it("should be that vampire if same vampire is used", () => {
+    it('should be that vampire if same vampire is used', () => {
       expect(offspring4.closestCommonAncestor(offspring4).name).to.equal(offspring4.name);
-    })
+    });
 
-    it("should be the more senior vampire if a direct ancestor is used", () => {
+    it('should be the more senior vampire if a direct ancestor is used', () => {
       expect(offspring6.closestCommonAncestor(offspring7).name).to.equal(offspring6.name);
       expect(offspring7.closestCommonAncestor(offspring6).name).to.equal(offspring6.name);
-    })
+    });
 
-    it("should be root for offspring 8 and offspring 7", () => {
+    it('should be root for offspring 8 and offspring 7', () => {
       expect(offspring7.closestCommonAncestor(offspring8).name).to.equal(rootVampire.name);
       expect(offspring8.closestCommonAncestor(offspring7).name).to.equal(rootVampire.name);
-    })
+    });
   });
 });
--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let counter = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      counter++;
+    }
+    return counter;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -35,10 +42,31 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  // closestCommonAncestor(vampire) {
+  //   //directLineWIth fxn
+  //   const directLineWith = (jrVampire, srVampire) => {
+  //     let currentVampire = jrVampire;
+  //     while (currentVampire.creator) {
+  //       if (currentVampire.creator === srVampire) {
+  //         return true;
+  //       }
+  //       currentVampire = currentVampire.creator;
+  //     }
+  //     return false;
+  //   };
 
-  }
+  //   if (vampire === this) {
+  //     return this;
+  //   }
+
+  //   let jrVampire = this.isMoreSeniorThan(vampire) ? vampire : this; //true or false
+  //   let srVampire = this.isMoreSeniorThan(vampire) ? this : vampire;
+  //   if (directLineWith(jrVampire, srVampire)) {
+  //     return srVampire;
+  //   } else {
+  //     return false;
+  //   }
+  // }
 }
 
 module.exports = Vampire;
-
